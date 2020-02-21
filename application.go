aa4acf12f5969c6ccec73cff38bfbacd5110542d
joy <joy.lai@zg.com>
2020-02-21 11:13:25 +0800

    v0.1.2

--- application.go
+++ application.go
@@ -1,10 +1,22 @@
 package go_application
 
-
 type ApplicationClass struct {
 	Debug bool
+	Env   string
 }
 
-var Application = ApplicationClass{true}
+func (app *ApplicationClass) SetEnv(env string) {
+	app.Env = env
+	app.Debug = IsDebug(env)
+}
+
+var Application = ApplicationClass{
+	Env: `dev`,
+	Debug: IsDebug(`dev`),
+}
 
 var OnTerminated = make(chan interface{})
+
+func IsDebug(env string) bool {
+	return env != `prod`
+}
