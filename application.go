e78cd9f14e9d96e83aff91e60ae87261e6666cba
joy <joy.lai@zg.com>
2020-03-19 19:54:38 +0800

    v0.1.3

--- application.go
+++ application.go
@@ -1,22 +1,37 @@
 package go_application
 
+import "context"
+
 type ApplicationClass struct {
-	Debug bool
-	Env   string
+	Debug  bool
+	Env    string
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 func (app *ApplicationClass) SetEnv(env string) {
 	app.Env = env
-	app.Debug = IsDebug(env)
+	app.Debug = isDebug(env)
 }
 
+// 退出应用
+func (app *ApplicationClass) Exit() {
+	app.cancel()
+}
+
+// 应用结束通道
+func (app *ApplicationClass) OnFinished() <-chan struct{} {
+	return app.ctx.Done()
+}
+
+var ctx, cancel = context.WithCancel(context.Background())
 var Application = ApplicationClass{
-	Env: `dev`,
-	Debug: IsDebug(`dev`),
+	Env:    `dev`,
+	Debug:  isDebug(`dev`),
+	ctx:    ctx,
+	cancel: cancel,
 }
 
-var OnTerminated = make(chan interface{})
-
-func IsDebug(env string) bool {
+func isDebug(env string) bool {
 	return env != `prod`
 }
